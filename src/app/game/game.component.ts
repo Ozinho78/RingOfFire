9d35b6df1401e0615b72b8956663a7c743870981
Michael Fiebelkorn <michael.fiebelkorn@gmail.com>
2024-12-23 23:44:21 +0100

    pick cards

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Game } from '../../models/game';
+import { PlayerComponent } from "../player/player.component";
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PlayerComponent],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
@@ -22,7 +23,7 @@ export class GameComponent {
 
   newGame(){
     this.game = new Game();
-    console.log(this.game);
+    // console.log(this.game);    // show template of created object
   }
   //   Game{players: Array(0), stack: Array(0), playedCard: Array(0), currentPlayer: 0}currentPlayer: 0playedCard: []players: []stack: [][[Prototype]]: Object
   // {
@@ -36,12 +37,16 @@ export class GameComponent {
   takeCard(){
     if(!this.pickCardAnimation){    // man kann draufklicken, wenn Animation wieder auf false zurückgesetzt wurde
       this.currentCard = this.game.stack.pop(); // remove the last element from the stack, pick last card from array
-      console.log(this.currentCard);
+      // console.log(this.currentCard);
       this.pickCardAnimation = true;
+      // console.log('New card: ' + this.currentCard); // print the card that was picked
+      // console.log('Game is: ', this.game);    // outlog game object
+      
 
       setTimeout(() => {      // nach 1,5 Sekunden wird die Animation wieder zurückgesetzt
+        this.game.playedCard.push(this.currentCard);  // push currentCard into stack of played cards; pushed within timeout in order to not show the played card before the animation of the picked card finishes
         this.pickCardAnimation = false;
-      }, 1500)
+      }, 1000)
     }
   }
 }
