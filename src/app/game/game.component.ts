8085fee8ab7c36e964576e3fe8dd01fddf9de2dd
Michael Fiebelkorn <michael.fiebelkorn@gmail.com>
2024-12-23 06:31:13 +0100

    class game created

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Game } from '../../models/game';
 
 @Component({
   selector: 'app-game',
@@ -10,8 +11,37 @@ import { CommonModule } from '@angular/common';
 })
 export class GameComponent {
   pickCardAnimation = false;
+  currentCard: string = '';
+  game: Game;
+
+  constructor(){}
+
+  ngOnInit(): void{
+    this.newGame();
+  }
+
+  newGame(){
+    this.game = new Game();
+    console.log(this.game);
+  }
+  //   Game{players: Array(0), stack: Array(0), playedCard: Array(0), currentPlayer: 0}currentPlayer: 0playedCard: []players: []stack: [][[Prototype]]: Object
+  // {
+  //   "players": [],
+  //   "stack": [],
+  //   "playedCard": [],
+  //   "currentPlayer": 0
+  // }
+
 
   takeCard(){
-    this.pickCardAnimation = true;
+    if(!this.pickCardAnimation){    // man kann draufklicken, wenn Animation wieder auf false zurückgesetzt wurde
+      this.currentCard = this.game.stack.pop(); // remove the last element from the stack, pick last card from array
+      console.log(this.currentCard);
+      this.pickCardAnimation = true;
+
+      setTimeout(() => {      // nach 1,5 Sekunden wird die Animation wieder zurückgesetzt
+        this.pickCardAnimation = false;
+      }, 1500)
+    }
   }
 }
